c31a7c769e655ef8577a12273a1eab2bdfb32842
Luck Mix <153753933+lk3140318@users.noreply.github.com>
2024-09-15 17:01:39 +0530

    Update config.py
    Signed-off-by: Luck Mix  <153753933+lk3140318@users.noreply.github.com>

--- config.py
+++ config.py
@@ -2,17 +2,17 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "24335028")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "b204ec833fb451fb913fc8e683b232d0")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7531498113:AAESwZ-J9SX92mvjO7risOzFYvnDrc727OM") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "WebXBots") 
 
-DB_NAME = os.environ.get("DB_NAME","")     
+DB_NAME = os.environ.get("DB_NAME","aadarshkumar1234768")     
 
-DB_URL = os.environ.get("DB_URL","")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://aadarshkumar1234768:Q8ptH5spkMkR93eg@cluster0.0ntbfcn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
